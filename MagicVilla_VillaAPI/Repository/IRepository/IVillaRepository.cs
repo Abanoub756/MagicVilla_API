9ad7d1b6d4547085efe5b1d5ddd3f91bf15029c7
Abanoub756 <abanoubsamy756@gmail.com>
2025-09-19 12:22:24 +0300

    Initial commit

---
+++ MagicVilla_VillaAPI/Repository/IRepository/IVillaRepository.cs
@@ -0,0 +1,13 @@
+﻿using MagicVilla_VillaAPI.Models;
+using MagicVilla_VillaAPI.Repository.IRepository;
+using System.Linq.Expressions;
+
+namespace MagicVilla_VillaAPI.Repository.Repository
+{
+    public interface IVillaRepository : IRepository<Villa>
+    {
+
+        Task<Villa> UpdateAsync(Villa entity);
+
+    }
+}
